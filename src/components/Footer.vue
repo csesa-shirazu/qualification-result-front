412f81a03af2b7fb3e1f5c19d9f652036c954584
Ali Mahdiyar <alimahdiyar77@gmail.com>
2019-01-19 23:07:47 +0330

    remove names from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,17 +4,11 @@
         <div class="eight wide column" style="text-align: right">
             <div style="text-align: center; float: right; margin-right: 10px">
                 <svg class="footer-logo" width="20" height="40" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 36 70"><defs></defs><desc>Generated with Avocode.</desc><g transform="matrix(1,0,0,1,-609,-994)"><g><title>Cnas</title><image xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAABGCAYAAACkPDbJAAABb0lEQVRoQ+XY0Q6CMAyFYRvf/5WdgQSCc1t3upYeo9dDPn/GYMqD7COenlJKEZGl71w6uP4xVKANswFpClGBDgxNISrQFUNRiApUY9ILUYFamNRCVKAeJq0QFWiESSlEBWotgqkLIxWoVyKtEBVo9IhIKTQ66e0g7QGaDqpf4m8FaXW2lTkVtLrFmdmUdjeKM3VmToCO+Q0QUkd72LoUogIhmNZdhha5jt9umq85RAVCMZ6FjjgfhSwg9BJpu5UT1Bn4EpEnetLReO1HayDYoq3mUyDPtWQE0jD7rsVzYmrboCmQZ50RaAazH08Hgmet4YDZOuccMpwDOoQeNLoTl//J115hkToulwwFaQtnaCG0TnghrV7rzggrZKkTWshS539A1jphhahA9Z2jrTtf46GHUmOw9rZABUIxIXOo3vihV8B9YTwAljqhhahAVkxYISrQCiakEBVoFeNeiArkgXEtRAXywrgVogKhD09t/BukT4lOa9bfdAAAAABJRU5ErkJggg==" width="36" height="70" transform="matrix(1,0,0,1,609,994)" ></image></g></g></svg>
-                <div style="font-size: 13px;">
-                    سینا سعیدی
-                </div>
             </div>
         </div>
         <div class="eight wide column" style="text-align: left">
             <div style="text-align: center; float: left">
                 <img style="height: 40px" src="/static/img/logo-csesa.png" />
-                <div style="font-size: 12px;">
-                    انجمن علمی کامپیوتر
-                </div>
             </div>
         </div>
     </div>
@@ -29,8 +23,8 @@
 <style>
 #footer {
     border-radius: 38px 38px 0 0;
-    width: 70%;
-    height: 80px;
+    width: 60%;
+    height: 70px;
     margin: auto;
 }
 .footer-logo image{
